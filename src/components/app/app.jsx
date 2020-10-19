511100992bca3984c4f80d3cc15a5de85e9e75de
Timka-Sawyer <timka-sawyer@yandex.ru>
2020-10-19 23:54:36 +0300

    Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import Main from "../main/main";
+import LoginScreen from "../login-screen/login-screen";
+import MyListScreen from "../my-list-screen/my-list-screen.jsx";
+import FilmScreen from "../film-screen/film-screen";
+import AddReviewScreen from "../add-review/add-reviwe";
+import PlayerScreen from "../player-screen/player-screen";
 import PropTypes from "prop-types";
-
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 const App = (props) => {
   return (
-    <Main
-      filmName = {props.filmName}
-      filmGenre = {props.filmGenre}
-      filmDate = {props.filmDate}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            filmName = {props.filmName}
+            filmGenre = {props.filmGenre}
+            filmDate = {props.filmDate} />
+        </Route>
+        <Route exact path="/login" >
+          <LoginScreen />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListScreen />
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmScreen />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen />
+        </Route>
+        <Route exaxt path="/player/:id">
+          <PlayerScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
